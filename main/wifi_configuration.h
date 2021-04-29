728f92c86470ddfa3fc8e1b1960a4e64ff38b60a
ike <38341483+was3912734@users.noreply.github.com>
2021-04-29 17:51:01 +0800

    Update wifi_configuration.h
    add wifi SSID

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -10,8 +10,8 @@
 #ifndef __WIFI_CONFIGURATION__
 #define __WIFI_CONFIGURATION__
 
-#define WIFI_SSID "DAP"
-#define WIFI_PASS "12345678"
+#define WIFI_SSID "HUAWEI-0G12OP"
+#define WIFI_PASS "3912734122813"
 
 #define PORT 3240
 
