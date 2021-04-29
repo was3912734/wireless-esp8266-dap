b273eea3b22d37e2fce8d88982533321385a5a50
ike <38341483+was3912734@users.noreply.github.com>
2021-04-29 19:14:52 +0800

    Update wifi_configuration.h
    ADD SSID

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -10,14 +10,14 @@
 #ifndef __WIFI_CONFIGURATION__
 #define __WIFI_CONFIGURATION__
 
-#define WIFI_SSID "DAP"
-#define WIFI_PASS "12345678"
+#define WIFI_SSID "HUAWEI-0G12OP"
+#define WIFI_PASS "3912734122813"
 
 
 #define USE_STATIC_IP 1
 // If you don't want to specify the ip configuration, then ignore the following items.
-#define DAP_IP_ADDRESS 192, 168, 137, 123
-#define DAP_IP_GATEWAY 192, 168, 137, 1
+#define DAP_IP_ADDRESS 192, 168, 3, 102
+#define DAP_IP_GATEWAY 192, 168, 3, 1
 #define DAP_IP_NETMASK 255, 255, 255, 0
 
 // DO NOT CHANGE
